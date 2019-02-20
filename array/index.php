5bb306914453fbd99aae4422ce43d66c6af9935c
Jansin3 <jan.sinilnikov@khk.ee>
2019-02-20 15:26:27 +0200

    update5

---
+++ array/index.php
@@ -0,0 +1,21 @@
+<?php
+$peppaPig = array(
+    'Peppa',
+    'naine',
+    4,
+    1.04
+);
+echo $peppaPig[0].'<br>';
+echo $peppaPig[1].'<br>';
+echo $peppaPig[2].'<br>';
+echo $peppaPig[3].'<br>';
+echo '<hr>';
+for($i = 0; $i < count($peppaPig); $i++) {
+    echo $peppaPig[$i].'<br>';
+}
+echo '<hr>';
+foreach ($peppaPig as $porsas) {
+    foreach ($porsas as $element) {
+        echo $element . '<br>';
+    }
+}
