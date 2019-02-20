2669b40274bbe9e25911df917bfd082344f6fa55
Jansin3 <jan.sinilnikov@khk.ee>
2019-02-20 12:25:51 +0200

    update

---
+++ functions/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jan.sinilnikov
+ * Date: 20.02.2019
+ * Time: 12:08
+ * defineerimine functioon */
+function paarsuseKontroll($arv){
+    if(arv % 2 ==0) {
+        echo $arv. 'on paaris<br>';
+    } else {
+        echo $arv. 'on paaritu<br>';
+    }
+}
+
+paarsuseKontroll(arv: 7);
